27343e5b15d99d85b02a5b069ba87c2b105bb354
johno11gh <84769597+johno11gh@users.noreply.github.com>
2021-11-22 02:26:26 +0900

    Update twitterUserSearch.py

--- twitterUserSearch.py
+++ twitterUserSearch.py
@@ -6,10 +6,10 @@
 
 #TwitterAPI
 import tweepy
-CK = 'MOUAnEZcVXbuymSvEq4GSPCqR' #consumer_key
-CS = 'kGcCpaKTTh5W3bR3f05gzpemhsbMmoTKvYvoYPSptAq4Dwnn8r' #consumer_secret
-AT = '1254808886667665409-doIXOMOrC1shBHUQUz5dHCvgQ0z9Nc' #Access_token
-AS = 'Vtjq1jEtsE4BUgv07aAquUUVhSh3bYNlUi4ISiBh6vEYz' #Access_token_secret
+CK = '' #consumer_key
+CS = '' #consumer_secret
+AT = '-' #Access_token
+AS = '' #Access_token_secret
 auth = tweepy.OAuthHandler(CK, CS)
 auth.set_access_token(AT, AS)
 
